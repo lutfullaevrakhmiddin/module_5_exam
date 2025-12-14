818cb135f809f5980d4b0af15621a608214c3515
pdp <lutfullaevrakhmiddin@gmail.com>
2025-12-14 16:19:04 +0500

    change fourth

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -7,5 +7,6 @@ import java.util.UUID;
 public class Main {
     public static void main(String[] args) {
         User user = new User(UUID.randomUUID(), "lutfullayevrahmiddin", "Rahmiddin", 22);
+        System.out.println(user);
     }
 }
