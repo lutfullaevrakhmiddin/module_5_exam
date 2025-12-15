c2ccae1e20a4eca71b8d2f061d496d664d876c1d
pdp <lutfullaevrakhmiddin@gmail.com>
2025-12-15 20:32:07 +0500

    change fifth

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         User user = new User(UUID.randomUUID(), "lutfullayevrahmiddin", "Rahmiddin", 22);
         System.out.println(user);
+//        there is another change in 15.12.2025
     }
 }
